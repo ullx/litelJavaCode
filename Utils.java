37cf5e34ec53b3ecb302ed766cef533f87255199
ulxulx <ulises.sci@gmail.com>
2016-06-29 14:49:11 -0500

    Moved some generic methods
    Removed some methods from WordFinder.java to Utils as they are kinda generic as they change the passed grid as an argument and work over it. And to better organize the classes.

--- Utils.java
+++ Utils.java
@@ -1,5 +1,6 @@
 package com.games;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
@@ -56,4 +57,82 @@ public class Utils {
 		return sb.toString();
 	}
 
+	
+	public static void printGrid(String[][] grid, boolean withCoords, ArrayList<Coordinate> markCoords) {
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < grid.length; i++) {
+			for (int j = 0; j < grid.length; j++) {
+				if(markCoords != null && lookCoordinate(markCoords, new Coordinate(i,j))) {
+					sb.append("*");
+				}
+				if(withCoords) {
+					sb.append(grid[i][j]).append("[").append(i).append(",").append(j).append("]");
+				} else {
+					sb.append(grid[i][j]);
+				}
+			}
+			sb.append("\n");
+		}
+		System.out.println(sb);
+	}
+	
+	private static boolean lookCoordinate(ArrayList<Coordinate> list, Coordinate coord) {
+		Iterator<Coordinate> it = list.iterator();
+		while(it.hasNext()) {
+			Coordinate co = it.next();
+			if(co.equals(coord)){
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public static void printGridWithCoords(String[][] grid) {
+		printGrid(grid, true, null);
+	}
+	
+	public static void printGridWithoutCoords(String[][] grid) {
+		printGrid(grid, false, null);
+	}
+	
+	public static void printAndMarkCoordsInGrid(String[][] grid, ArrayList<Coordinate> toRemove) {
+		printGrid(grid, true, toRemove);
+	}
+
+	public ArrayList<String> getIndexesByLetter(Character[][] grid, Character ch) {
+		
+		ArrayList<String> indexes = new ArrayList<String>();
+		for (int i = 0; i < grid.length; i++) {
+			for (int y = 0; y < grid[i].length; y++) {
+				if(grid[i][y].equals(ch)) {
+					indexes.add(i + "," + y);
+				}
+			}
+		}
+		return indexes;
+	}
+
+	public static void printFlatGrid(String[][] grid) {
+		StringBuilder sb = new StringBuilder();
+		for(int i =0; i < grid.length; i ++) {
+			for(int j =0; j < grid.length; j ++) {
+				sb.append(grid[i][j]);
+			}
+		}
+		System.out.println(sb);
+	}
+
+	public static ArrayList<Coordinate> getIndexesByLetter(Character ch, String[][] grid) {
+//		printLog("Getting indexes by letter " + ch);
+		ArrayList<Coordinate> indexes = new ArrayList<Coordinate>();
+		for (int idx = 0; idx < grid.length; idx++) {
+			for (int idy = 0; idy < grid[idx].length; idy++) {
+				if (grid[idx][idy].equals(ch + "")) {
+					indexes.add(new Coordinate(idx, idy));
+				}
+			}
+		}
+		return indexes;
+	}
+	
 }
